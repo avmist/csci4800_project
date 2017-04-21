5dee4d3f04efbc80027d71c468e70151826022ad
Ava Mistry <ava.mistry@ucdenver.edu>
2017-04-21 12:08:24 -0600

    finished back-end functions

--- nessus.py
+++ nessus.py
@@ -2,42 +2,37 @@ import requests
 import json
 import getpass
 
-base_url = ''
-username = ''
-password = ''
-login = {}
+login = {} # JSON object for requesting token
 
-class scan():
-    def __init__(self,name,id):
-        self.name = name
-        self.id = id
+# initialize login JSON object for requesting token
+def login(ip,un,pw,port):
+    base_url = 'https://' + ip + ':' + port
+    login = {'username':un,'password':pw}
+
+# get token with login
 def get_token():
-    valid = True
     resp = requests.post(base_url+'/session',data=login,verify=False)
     return resp.json()['token']
 
+# get a list of all scans
 def get_scans():
-    ret_scans = []
+    scan_names = []
     token = get_token()
     headers = {'X-Cookie': 'token=' + token, 'content-type': 'application/json'}
-    resp = requests.get(base_url + '/scans', data=None, headers=headers, verify=False)
-    scans = resp.json()['scans']
-    for i in scans:
-        ret_scans.append(scan(name=i['id'],id=i['id']))
-    return ret_scans
+    scans = requests.get(base_url + '/scans', data=None, headers=headers, verify=False).json()['scans']
+    for scan in scans:
+        scan_names.append(scan['name'])
+    return scan_names
 
-def get_scan_data(id):
+# given a scan name, get a list of all items in that scan
+def get_scan_data(name):
+    scan_items = []
     token = get_token()
     headers = {'X-Cookie': 'token=' + token, 'content-type': 'application/json'}
-    resp = requests.get(base_url + '/scans/' + str(id), data=None, headers=headers, verify=False)
-	return list_of_stuff
-
-def send_data(list_of_chosen_data):
-
-
-
-def login(url,un,pw,port):
-	base_url = 'https://'+url+':'+port
-    username = un 
-    password = pw
-    login = {'username': username, 'password': password}
+    scans = requests.get(base_url + '/scans', data=None, headers=headers, verify=False).json()['scans']
+    for scan in scans:
+        if scan['name'] == name:
+            id = scan['id']
+    scan_data = requests.get(base_url + '/scans/' + str(id), data=None, headers=headers, verify=False)
+    for scan_item in scan_data:
+        scan_items.append(scan_item)
